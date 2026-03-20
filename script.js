5f0f0244e548a4d6866bc32726c752d71391f155
Phanindra SSDL <ssdlphani@gmail.com>
2026-03-20 11:29:37 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,33 @@
+// Store a reference to the <h1> in a variable
+const myHeading = document.querySelector("h1");
+myHeading.textContent = "My First Project";
+
+const myImage = document.querySelector("img");
+myImage.addEventListener("click", () => {
+    const mySrc = myImage.getAttribute("src");
+    if (mySrc === "D:\\SRH To Do Clock\\images\\sunrisers hyderabad.png") {
+        myImage.setAttribute("src", "D:\\SRH To Do Clock\\images\\srh jersey.jpg");
+    } else {
+        myImage.setAttribute("src", "D:\\SRH To Do Clock\\images\\sunrisers hyderabad.png");
+    }
+});
+
+let myButton = document.querySelector("button");
+
+function setUserName() {
+    const myName = prompt("Please enter your name.");
+    if (!myName) return; // guard against null/empty
+    localStorage.setItem("name", myName);
+    myHeading.textContent = `My First Project, ${myName}`;
+}
+
+if (!localStorage.getItem("name")) {
+    setUserName();
+} else {
+    const storedName = localStorage.getItem("name");
+    myHeading.textContent = `My First Project, ${storedName}`;
+}
+
+myButton.addEventListener("click", () => {
+    setUserName();
+});
